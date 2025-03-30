6347ca4bfcd1eb63892f630712c2985a4f5199ff
vadinvadya <vadim181@gmail.com>
2025-03-30 22:35:17 +0300

    file

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Streamlit задание")
+
+st.write("## Модели классификаци животных🙊 и фрутктов🍐")
+st.image('https://i.pinimg.com/736x/e5/91/f6/e591f671b592db08acd13f4580e9b6e6.jpg')
+
+#
